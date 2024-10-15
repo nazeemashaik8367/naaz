4069888f50417e3eddd8b4248f0ffd9416066ff9
nazeemashaik8367 <naziya1016@gmail.com>
2024-10-15 19:08:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
+
 provider "aws" {
+region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-  ami           = "ami-049332278e728bdb7"
-  instance_type = "t2 micro"
-  tags = {
-    instance_name = "naazinstance"
-  }
+count = 2
+ami = "ami-049332278e728bdb7"
+instance_type = "t2.micro"
 }
